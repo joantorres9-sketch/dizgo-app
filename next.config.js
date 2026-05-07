2b526ed34251bb21c3d237654b55321fb35a71e4
joantorres9-sketch <joantorres9@gmail.com>
2026-05-07 17:43:12 -0500

    Fix: agregar carpeta public v3

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**.supabase.co' },
-    ],
-  },
+  output: 'standalone',
 }
 module.exports = nextConfig
